f95437ab1b0101b087887af49c39640195306506
juan cruz aparicio <apariciojc807@gmail.com>
2024-04-25 21:12:04 -0300

    html, css y js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
-const nombreUsuario = prompt('ingrese su Nombre!!!')
-alert(`Hola ${nombreUsuario}, como te va`)
+const botonIniciarSesion = document.querySelector('#iniciar-sesion')
+const botonRegistrarse = document.querySelector('#registrarse')
+const botonCargar = document.querySelector('#cargar')
+const form = document.querySelector('#inicio-de-sesion')
+
+botonIniciarSesion.onclick = function() {
+    botonIniciarSesion.className = 'oculto'
+    botonRegistrarse.className = 'oculto'
+    form.className = ''
+}
+
+botonCargar.onclick = function() {
+    const user = document.querySelector('#user').value
+    const password = document.querySelector('#password').value
+
+    inicioDeSesion(user,password)
+}
+
+function inicioDeSesion(user, password) {
+    if(user){
+        if(password){
+            alert(user, password)
+        }
+    }
+}
